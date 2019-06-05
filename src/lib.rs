6b9d5f95ee482ae699758ff90c0c66fb1f633554
pierre <dommerc.pierre@gmail.com>
2019-06-05 02:12:08 +0200

    wip

--- src/lib.rs
+++ src/lib.rs
@@ -1,130 +1,101 @@
-pub mod parser {
+pub mod cli_parser {
+    pub struct Flag(pub &'static str, pub char, pub &'static str, pub bool);
 
     #[derive(Debug)]
-    pub struct Arg<T>(&'static str, &'static str, T)
-        where T: Fn(&mut Config, &mut std::env::Args);
+    pub struct Token(&'static str, Option<String>);
 
-    #[derive(Debug)]
-    pub enum Mod {
-        Normal,
-        Word,
-        Regex
-    }
-
-    #[derive(Debug)]
-    pub struct Config {
-        pub query: Option<String>,
-        pub replace: Option<String>,
-        pub search_mod: Mod,
-        pub case_sensitive: bool,
-        pub confirm: bool,
-        pub excluded: Option<Vec<String>>,
-        pub file_mask: Option<Vec<String>>,
-        pub root: Option<String>,
-        pub recursive: bool
-    }
-
-    #[derive(Debug)]
-    pub enum TokenType {
-        IgnoreCase,
-        WordMod,
-        RegexMod,
-        Confirm,
-        Excluded,
-        FileMask,
-        File,
-        Recursive,
-        Help,
-        Verbose,
-        Arg
-    }
-
-    impl Copy for TokenType { }
-    impl Clone for TokenType {
-        fn clone(&self) -> TokenType {
-            *self
-        }
-    }
-
-    pub struct OptData(TokenType, char, &'static str, bool);
-    const OPTIONS: [OptData; 10] = [
-        OptData(TokenType::IgnoreCase, 'i', "ignore-case", false),
-        OptData(TokenType::WordMod, 'w', "word", false),
-        OptData(TokenType::RegexMod, 'x', "regex", false),
-        OptData(TokenType::Confirm, 'c', "confirm", false),
-        OptData(TokenType::Excluded, 'e', "excluded", true),
-        OptData(TokenType::FileMask, 'm', "mask", true),
-        OptData(TokenType::File, 'f', "file", true),
-        OptData(TokenType::Help, 'h', "help", false),
-        OptData(TokenType::Verbose, 'v', "verbose", false),
-        OptData(TokenType::Recursive, 'r', "recursive", false)
-    ];
-
-    #[derive(Debug)]
-    pub struct Token(TokenType, Option<String>);
-
-    fn find_option<'a>(short: Option<char>, long: Option<&'a str>) -> Result<&'a OptData, &'static str> {
-        for option in &OPTIONS {
+    fn find_option<'a>(
+        options: &'a [Flag],
+        short: Option<char>,
+        long: Option<&'a str>,
+    ) -> Result<&'a Flag, &'static str> {
+        for option in options {
             if let Some(i) = short {
                 if i == option.1 {
-                    return Ok(option)
+                    return Ok(option);
                 }
             }
             if let Some(i) = long {
                 if i == option.2 {
-                    return Ok(option)
+                    return Ok(option);
                 }
             }
         }
         Err("unknown option")
     }
 
-    fn parse_opt(args: &mut std::env::Args, arg: &str, tokens: &mut Vec<Token>) -> Result<(), &'static str> {
-        let mut current_arg = &arg[1..];
+    fn parse_opt(
+        args: &mut std::env::Args,
+        options: &[Flag],
+        arg: &str,
+        tokens: &mut Vec<Token>,
+    ) -> Result<(), String> {
+        let current_arg = &arg[1..];
         for (i, c) in current_arg.char_indices() {
-            let option = find_option(Some(c), None)?;
-            if option.3 == false {
-                tokens.push(Token(option.0, None));
-            } else {
-                let arg_opt = &current_arg[i..];
+            let option = find_option(options, Some(c), None)
+                .map_err(|_| format!("flag unknown \"{}\"", c))?;
+            if option.3 == true {
+                let arg_opt = &current_arg[i + 1..];
                 if arg_opt.len() > 0 {
-                    tokens.push(Token(option.0, Some(String::from(arg_opt))))
+                    tokens.push(Token(option.0, Some(String::from(arg_opt))));
+                    break;
+                } else {
+                    match args.next() {
+                        Some(str) => {
+                            tokens.push(Token(option.0, Some(String::from(str))));
+                            return Ok(());
+                        }
+                        None => return Err(format!("argument missing for flag \"{}\"", option.0)),
+                    }
                 }
-
+            } else {
+                tokens.push(Token(option.0, None));
             }
         }
         Ok(())
     }
 
-    fn parse_long_opt(arg: &str, tokens: &mut Vec<Token>) -> Result<(), &'static str> {
+    fn parse_long_opt(
+        args: &mut std::env::Args,
+        options: &[Flag],
+        arg: &str,
+        tokens: &mut Vec<Token>,
+    ) -> Result<(), String> {
         let current_arg = &arg[2..];
         match current_arg.find("=") {
             None => {
-                let option = find_option(None, Some(&current_arg))?;
-                if option.3 == false {
-                    tokens.push(Token(option.0, None));
+                let option = find_option(options, None, Some(&current_arg))
+                    .map_err(|_| format!("flag unknown \"{}\"", current_arg))?;
+                if option.3 == true {
+                    match args.next() {
+                        Some(str) => {
+                            tokens.push(Token(option.0, Some(String::from(str))));
+                            return Ok(());
+                        }
+                        None => return Err(format!("argument missing for flag \"{}\"", option.0)),
+                    }
                 } else {
-                    return Err("bad synthax")
+                    tokens.push(Token(option.0, None));
                 }
-            },
+            }
             Some(i) => {
                 let first = &current_arg[..i];
-                let last = &current_arg[i+1..];
+                let last = &current_arg[i + 1..];
                 if first.len() == 0 || last.len() == 0 {
-                    return Err("bad synthax for option")
+                    return Err(format!("bad syntax for flag \"{}\"", current_arg));
                 }
-                let option = find_option(None, Some(first))?;
+                let option = find_option(options, None, Some(first))?;
                 if option.3 == true {
-                    tokens.push(Token(option.0, Some(String::from(last))))
+                    tokens.push(Token(option.0, Some(String::from(last))));
                 } else {
-                    return Err("bad synthax")
+                    return Err(format!("bad syntax for flag \"{}\"", current_arg));
                 }
             }
         }
         Ok(())
     }
 
-    fn tokenize(args: &mut std::env::Args, config: &mut Config) -> Result<(), &'static str> {
+    pub fn tokenize(mut args: std::env::Args, options: &[Flag]) -> Result<Vec<Token>, String> {
         let mut tokens: Vec<Token> = vec![];
         let mut accept_opt = true;
         args.next(); //skip the binary
@@ -132,116 +103,13 @@ pub mod parser {
             if arg.len() == 2 && &arg[..2] == "--" {
                 accept_opt = false;
             } else if arg.len() > 2 && &arg[..2] == "--" && accept_opt == true {
-                parse_long_opt(&arg, &mut tokens)?;
+                parse_long_opt(&mut args, options, &arg, &mut tokens)?;
             } else if arg.len() > 1 && &arg[..1] == "-" && accept_opt == true {
-                parse_opt(args, &arg, &mut tokens)?;
+                parse_opt(&mut args, options, &arg, &mut tokens)?;
             } else {
-                println!("arg");
-                tokens.push(Token(TokenType::Arg, Some(String::from(arg))));
+                tokens.push(Token("arg", Some(String::from(arg))));
             }
         }
-        dbg!(tokens);
-        Ok(())
-    }
-
-    impl Config {
-        pub fn new(mut args: std::env::Args) -> Result<Config, &'static str> {
-            if args.len() < 1 {
-                return Err("not enough arguments");
-            }
-            let mut config = Config {
-                query: None,
-                replace: None,
-                search_mod: Mod::Normal,
-                case_sensitive: false,
-                confirm: true,
-                excluded: None,
-                file_mask: None,
-                root: None,
-                recursive: false
-            };
-            tokenize(&mut args, &mut config)?;
-            Ok(config)
-        }
+        Ok(tokens)
     }
 }
-
-
-// const ARGS: [Arg<fn(&mut Config, &mut std::env::Args)>; 2] = [
-// Arg("i", "ignore-case", |config, _args| {
-// config.case_sensitive = true;
-// }),
-// Arg("c", "confirm", |config, _args| {
-// config.confirm = true;
-// })
-// ];
-//
-// fn arg_handler(arg: &String, config: &mut Config, args: &mut env::Args) -> Result<(), &'static str> {
-// if config.query == None {
-// config.query = Some(arg.to_string());
-// return Ok(())
-// }
-// if config.replace == None {
-// config.replace = Some(arg.to_string());
-// return Ok(())
-// }
-// Err("bad usage")
-// }
-//
-// fn parse_long_opt(arg: &String, config: &mut Config, args: &mut env::Args) -> Result<(), &'static str> {
-// return match ARGS.iter().find(|&valid| valid.1 == arg) {
-// None => Err("option not found"),
-// Some(elem) => {
-// elem.2(config, args);
-// Ok(())
-// }
-// }
-// }
-//
-// fn option_handler(arg: &String, config: &mut Config, args: &mut env::Args) -> Result<(), &'static str> {
-// let vec_args: Vec<String> = env::args().skip(1).collect();
-// println!("{:#?}", vec_args);
-// if &arg[..2] == "--" {
-// parse_long_opt(&arg, config, args)?;
-// return Ok(())
-// } else {
-//
-// }
-// Ok(())
-// }
-//
-// impl Config {
-// pub fn new(mut args: std::env::Args) -> Result<Config, &'static str> {
-// args.next(); //skip the binary
-// if args.len() < 1 {
-// return Err("not enough arguments");
-// }
-// let mut config = Config {
-// query: None,
-// replace: None,
-// search_mod: Mod::Normal,
-// case_sensitive: false,
-// confirm: true,
-// excluded: None,
-// file_mask: None,
-// root: None,
-// recursive: false
-// };
-// let mut accept_opt = true;
-// while let Some(arg) = args.next() {
-// // println!("{:#?}", ARGS[0].2(&mut config, &mut args));
-// if arg.len() == 2 && &arg[..2] == "--" {
-// println!("terminate arg");
-// accept_opt = false;
-// } else if &arg[..1] == "-" && accept_opt == true {
-// println!("option");
-// option_handler(&arg, &mut config, &mut args)?;
-// } else {
-// println!("arg");
-// arg_handler(&arg, &mut config, &mut args)?;
-// }
-// }
-// Ok(config)
-// }
-//
-// }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,33 @@
+use seap::cli_parser::{self, Flag};
 use std::env;
+use std::io;
 use std::process;
-use seap::parser::Config;
+use termion::raw::IntoRawMode;
+use tui::backend::TermionBackend;
+use tui::layout::{Constraint, Direction, Layout};
+use tui::widgets::{Block, Borders, Widget};
+use tui::Terminal;
 
-fn main() {
-    let mut config = Config::new(env::args()).unwrap_or_else(|err| {
+const OPTIONS: [Flag; 2] = [
+    Flag("help", 'h', "help", false),
+    Flag("verbose", 'v', "verbose", false),
+];
+
+fn main() -> Result<(), io::Error> {
+    let tokens = cli_parser::tokenize(env::args(), &OPTIONS).unwrap_or_else(|err| {
         eprintln!("parser error: {}", err);
         process::exit(1);
     });
+    dbg!(&tokens);
+    let stdout = io::stdout().into_raw_mode()?;
+    let backend = TermionBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+    // terminal.draw(|mut f| {
+    // let size = f.size();
+    // Block::default()
+    // .title("Block")
+    // .borders(Borders::ALL)
+    // .render(&mut f, size);
+    // })
+    Ok(())
 }
